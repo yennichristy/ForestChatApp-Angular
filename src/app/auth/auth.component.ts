8f89041de187eb2c58981aaee0e397eea77f382e
Yenni Permatasari <yennikristianip@gmail.com>
2020-04-30 03:35:06 +0700

    [Yenni] - Slicing, styling auth page and switching(sign in & sign up) function

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.scss']
+  styleUrls: ['./auth.component.scss'],
 })
 export class AuthComponent implements OnInit {
+  signinPage: boolean = true;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  onSwitch() {
+    this.signinPage = !this.signinPage;
   }
 
+  onSubmit(form: NgForm) {
+    console.log(form.value);
+    form.reset();
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
