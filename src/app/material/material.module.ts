3fed89cd7fc9989fedaa5b8f323f48f80e740ed0
Yenni Permatasari <yennikristianip@gmail.com>
2020-05-06 12:20:39 +0700

    [Yenni] - Add auto complete module

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -4,12 +4,14 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 
 const MaterialComponents = [
   MatFormFieldModule,
   MatInputModule,
   MatButtonModule,
   MatIconModule,
+  MatAutocompleteModule,
 ];
 
 @NgModule({
