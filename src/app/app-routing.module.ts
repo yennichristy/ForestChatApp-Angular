ac31dd803371f363ddc04f3a049818464ba6b163
Yenni Permatasari <yennikristianip@gmail.com>
2020-05-06 12:16:52 +0700

    [Yenni] - Add new User route and children route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
+import { UserComponent } from './user/user.component';
+import { ChatStartComponent } from './user/chat-start/chat-start.component';
+import { ChatMessageComponent } from './user/chat-message/chat-message.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: AuthComponent },
+  {
+    path: 'user',
+    component: UserComponent,
+    children: [
+      { path: '', component: ChatStartComponent },
+      { path: ':id', component: ChatMessageComponent },
+    ],
+  },
 ];
 
 @NgModule({
