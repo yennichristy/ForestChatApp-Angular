17bdd8f7317715e8bd1f7f473404a4f3a691fe2f
Yenni Permatasari <yennikristianip@gmail.com>
2020-05-07 01:10:25 +0700

    [Yenni] - Add search function

--- src/app/user/chat-list/chat-list.component.ts
+++ src/app/user/chat-list/chat-list.component.ts
@@ -9,10 +9,23 @@ import { ChatService } from '../chat.service';
 })
 export class ChatListComponent implements OnInit {
   chat: ChatList[];
+  name: string;
 
   constructor(private chatService: ChatService) {}
 
   ngOnInit(): void {
     this.chat = this.chatService.getChatList();
+  }
+
+  search() {
+    if (this.name != '') {
+      this.chat = this.chat.filter((res) => {
+        return res.name
+          .toLocaleLowerCase()
+          .match(this.name.toLocaleLowerCase());
+      });
+    } else if (this.name == '') {
+      this.ngOnInit();
+    }
   }
 }
