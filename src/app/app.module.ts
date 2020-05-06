61d1a7e4207b37ea9722c71dfdee223b2e97324a
Yenni Permatasari <yennikristianip@gmail.com>
2020-05-06 12:18:36 +0700

    [Yenni] - New components for main interfaces

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,30 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 import { FormsModule } from '@angular/forms';
 import { AuthComponent } from './auth/auth.component';
+import { UserComponent } from './user/user.component';
+import { ChatListComponent } from './user/chat-list/chat-list.component';
+import { ChatDetailsComponent } from './user/chat-list/chat-details/chat-details.component';
+import { UserNavComponent } from './user/user-nav/user-nav.component';
+import { ChatMessageComponent } from './user/chat-message/chat-message.component';
+import { ContactInfoComponent } from './user/chat-message/contact-info/contact-info.component';
+import { MessageComponent } from './user/chat-message/message/message.component';
+import { InputMessageComponent } from './user/chat-message/input-message/input-message.component';
+import { ChatStartComponent } from './user/chat-start/chat-start.component';
 
 @NgModule({
-  declarations: [AppComponent, AuthComponent],
+  declarations: [
+    AppComponent,
+    AuthComponent,
+    UserComponent,
+    ChatListComponent,
+    ChatDetailsComponent,
+    UserNavComponent,
+    ChatMessageComponent,
+    ContactInfoComponent,
+    MessageComponent,
+    InputMessageComponent,
+    ChatStartComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
