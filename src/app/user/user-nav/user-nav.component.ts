b7c5e1420dfae036ff21dfe4a8755f83955f94d1
Yenni Permatasari <yennikristianip@gmail.com>
2020-05-09 03:54:44 +0700

    [Yenni] - Inject user service, add configuation for user info with mat dialog

--- src/app/user/user-nav/user-nav.component.ts
+++ src/app/user/user-nav/user-nav.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { User } from '../../models/user.model';
+import { UserService } from '../services/user.service';
+import { UserInfoComponent } from '../user-info/user-info.component';
 
 @Component({
   selector: 'app-user-nav',
   templateUrl: './user-nav.component.html',
-  styleUrls: ['./user-nav.component.scss']
+  styleUrls: ['./user-nav.component.scss'],
 })
 export class UserNavComponent implements OnInit {
+  user: User;
 
-  constructor() { }
+  constructor(public dialog: MatDialog, private userService: UserService) {}
 
   ngOnInit(): void {
+    this.user = this.userService.userInfo;
   }
 
+  openUserInfo() {
+    const userDialog = this.dialog.open(UserInfoComponent, {
+      width: '500px',
+      data: {
+        name: this.user.nameUser,
+        phone: this.user.phoneUser,
+        picture: this.user.photoUser,
+      },
+    });
+
+    userDialog.afterClosed().subscribe((result) => {
+      console.log('Changed has made');
+      this.user = result;
+    });
+  }
 }
