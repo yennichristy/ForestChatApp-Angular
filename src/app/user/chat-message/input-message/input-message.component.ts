056d7002e30624859156b0a6073613189c77ce17
Yenni Permatasari <yennikristianip@gmail.com>
2020-05-12 01:29:50 +0700

    [Yenni] - Input sending message(but still cant be push/post)

--- src/app/user/chat-message/input-message/input-message.component.ts
+++ src/app/user/chat-message/input-message/input-message.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-input-message',
   templateUrl: './input-message.component.html',
-  styleUrls: ['./input-message.component.scss']
+  styleUrls: ['./input-message.component.scss'],
 })
 export class InputMessageComponent implements OnInit {
+  @Output() myMsg: EventEmitter<any> = new EventEmitter();
+  chatId: number;
+  message: string;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  sendMsg(text: NgForm) {
+    this.route.params.subscribe((params) => {
+      const idChat = params.id;
+      this.chatId = parseInt(idChat);
+    });
+
+    text.value.chatId = this.chatId;
+    this.chatId = text.value.chatId;
+    const message = text.value.message;
+
+    const msgSend = {
+      chatId: this.chatId,
+      message,
+    };
+    this.myMsg.emit(msgSend);
+    text.reset();
   }
 
+  handleSendMsg(event) {
+    if (event.keyCode === 13) {
+      this.sendMsg;
+    }
+  }
 }
