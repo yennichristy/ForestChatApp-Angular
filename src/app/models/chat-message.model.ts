465b3ddd6487284cc26d52f687e1b661addca17f
Yenni Permatasari <yennikristianip@gmail.com>
2020-05-12 01:15:57 +0700

    [Yenni] - Modified ChatMessage model

--- src/app/models/chat-message.model.ts
+++ src/app/models/chat-message.model.ts
@@ -1,12 +1,7 @@
 export class ChatMessage {
   constructor(
-    public idMsg: string,
-    public bodyMsg: string,
-    public timeMsg: string,
-    public dateMsg: Date = new Date(),
-    public statusMsg: string,
-    public idConv: string,
-    public idGroup: string,
-    public idReceiver: string
+    public content: string,
+    public contentReceive: string,
+    public dateMsg: Date = new Date()
   ) {}
 }
