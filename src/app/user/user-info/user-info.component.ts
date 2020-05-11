4dd04436ed30d32a14e35fd61661a986ee4288cf
Yenni Permatasari <yennikristianip@gmail.com>
2020-05-12 01:25:44 +0700

    [Yenni] - Add dialog configuration and close function

--- src/app/user/user-info/user-info.component.ts
+++ src/app/user/user-info/user-info.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { UserService } from '../services/user.service';
 import { User } from 'src/app/models/user.model';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-user-info',
@@ -10,9 +11,17 @@ import { User } from 'src/app/models/user.model';
 export class UserInfoComponent implements OnInit {
   user: User;
 
-  constructor(private userService: UserService) {}
+  constructor(
+    public dialogRef: MatDialogRef<UserInfoComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: User,
+    private userService: UserService
+  ) {}
 
   ngOnInit(): void {
     this.user = this.userService.userInfo;
+  }
+
+  onClose() {
+    this.dialogRef.close();
   }
 }
