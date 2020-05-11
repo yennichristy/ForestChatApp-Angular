5909bd0be2928d49171f6750fdf940ca0ec2070b
Yenni Permatasari <yennikristianip@gmail.com>
2020-05-12 01:21:01 +0700

    [Yenni] - Add static data for message service

--- src/app/user/services/message.service.ts
+++ src/app/user/services/message.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { ChatMessage } from 'src/app/models/chat-message.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MessageService {
+  message: ChatMessage;
+  myDate = Date.now();
 
-  constructor() { }
+  public msgContent: ChatMessage = new ChatMessage(
+    'Hi! How are you doing?',
+    'Doing good. You?'
+  );
+
+  constructor() {}
 }
